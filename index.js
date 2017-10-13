d6d0dd94987454da445b5b43a6c5d46cfba1f89c
mohsinulhaq <mohammad.mohsin@sprinklr.com>
2017-10-13 22:39:26 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,68 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+mongoose.connect('mongodb://localhost/mohsinulhaq', { useMongoClient: true });
+mongoose.Promise = global.Promise;
+
+const db = mongoose.model('todo', mongoose.Schema({ todo: String, done: Boolean }));
+
+app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.json());
+
+app.get('/api', (req, res) => {
+    db.find((err, todos) => {
+        if (err) res.send(err);
+
+        res.json(todos);
+    });
+});
+
+app.post('/api', (req, res) => {
+    db.create({
+        todo: req.body.todo,
+        done: false
+    }, (err) => {
+        if (err) res.send(err);
+
+        db.find((err, todos) => {
+            if (err) res.send(err);
+
+            res.json(todos);
+        });
+    });
+});
+
+app.put('/api/:todo_id', (req, res) => {
+    db.update({
+            _id: req.params.todo_id
+        }, {
+            done: req.body.action === 'done'
+        }, (err) => {
+            if (err) res.send(err);
+
+            db.find((err, todos) => {
+                if (err) res.send(err);
+
+                res.json(todos);
+            });
+        });
+});
+
+app.delete('/api/:todo_id', (req, res) => {
+    db.remove({
+        _id: req.params.todo_id
+    }, (err) => {
+        if (err) res.send(err);
+
+        db.find((err, todos) => {
+            if (err) res.send(err);
+
+            res.json(todos);
+        });
+    });
+});
+
+app.listen((process.env.PORT || 5000));
